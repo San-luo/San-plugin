204cbd011e2b8f32a9d4745c8da1c1fd0de4fd03
San散 <3126986875@qq.com>
2024-12-01 16:49:03 +0800

    优化:天气地区名改为正则捕获

--- apps/weather.js
+++ apps/weather.js
@@ -9,7 +9,7 @@ export class San_Weather extends plugin {
         priority: cfg_priority,//优先级
         rule: [
             { 
-          reg: '^#?[\u4e00-\u9fa5A-Za-z]+天气$',
+          reg: '^#?(.*)天气$',
           fnc: 'weather_info'
                 // 执行方法
             }   
@@ -19,12 +19,10 @@ export class San_Weather extends plugin {
     }
     async weather_info (e){
         let str = e.msg
-        // 获取 # 之后的第一个位置
-        let startIndex = str.indexOf("#") + 1;
-        // 获取 "天气" 之前的位置
-        let endIndex = str.indexOf("天气");
-        // 提取子字符串
-        let location = str.substring(startIndex, endIndex);
+        let reg = /^^#?(.*)天气$/
+        let match = str.match(reg)
+        //logger.error(match)
+        let location = match[1]
         // 获取对应的和风天气url
         let url = await tool.location_url(location)
         // 截取指定区域
