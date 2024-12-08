d7eb3ededd0159bf3f4499b84abb4294e5cb53e0
root <root@localhost.localdomain>
2024-12-08 23:54:53 +0800

    适配trss

--- apps/set-cfg.js
+++ apps/set-cfg.js
@@ -21,10 +21,10 @@ export class San_SetCfg extends plugin {
   
     }
     async SetCfg (e) {
-        if (e.user_id != config.masterQQ[0]){
-            e.reply("你不是我的主人哦")
+        if (!tool.ismaster(e.user_id)) {
+            e.reply('你不是我的主人哦')
             return false
-        } 
+        }
         let NumberReg = /^#?(散|san|San)设置([\u4e00-\u9fa5]*)?(-?\d*)?$/
         let str = e.msg
         const NumberMatch = str.match(NumberReg)
