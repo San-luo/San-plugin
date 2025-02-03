4522c72bb2c75526f20bd3cc772b2c8787272af8
“San-luo” <3126986875@qq.com>
2025-02-03 22:41:08 +0800

    适配锅巴喽,相关配置可到锅巴上进行更改

---
+++ guoba.support.js
@@ -0,0 +1,169 @@
+import path from 'path'
+import lodash from 'lodash'
+import * as tool from './models/tool.js';
+
+// 支持锅巴
+export function supportGuoba () {
+  return {
+    // 插件信息，将会显示在前端页面
+    // 如果你的插件没有在插件库里，那么需要填上补充信息
+    // 如果存在的话，那么填不填就无所谓了，填了就以你的信息为准
+    pluginInfo: {
+      // name 为插件唯一标识，尽量不要与其他插件重复
+      name: 'San-plugin',
+      // title 为显示名称
+      title: 'San-Plugin',
+      // 插件描述
+      description: '业余写的小插件,覆盖云崽原有添加功能',
+      // 作者可以为字符串也可以为数组，当有多个作者时建议使用数组
+      author: [
+        '@san-luo'
+      ],
+      // 作者主页地址。若author为数组，则authorLink也需要为数组，且需要与author一一对应
+      authorLink: [
+        'https://gitee.com/San-luo'
+      ],
+      // 仓库地址
+      link: 'https://gitee.com/San-luo/San-plugin',
+      isV3: true,
+      isV2: false,
+      // 是否显示在左侧菜单，可选值：auto、true、false
+      // 当为 auto 时，如果配置项大于等于 3 个，则显示在左侧菜单
+      showInMenu: 'auto',
+      // 显示图标，此为个性化配置
+      // 图标可在 https://icon-sets.iconify.design 这里进行搜索
+      //icon: 'mdi:stove',
+      // 图标颜色，例：#FF0000 或 rgb(255, 0, 0)
+      //iconColor: '#d19f56',
+      // 如果想要显示成图片，也可以填写图标路径（绝对路径）
+      iconPath: `${path.resolve("./plugins/San-plugin/resources/img/icon.png")}`
+    },
+    // 配置项信息
+    configInfo: {
+      // 配置项 schemas
+      schemas: [
+        {
+            label: '功能配置',
+            // 第一个分组标记开始，无需标记结束
+            component: 'SOFT_GROUP_BEGIN'
+        },
+        {
+            field: 'config.imgQuality',
+            label: '图像质量',
+            helpMessage: '帮助图,天气图等',
+            bottomHelpMessage: '可选范围为1~100,建议80+,较低会出现马赛克',
+            // 【组件类型】，可参考
+            // https://doc.vvbin.cn/components/introduction.html
+            // https://3x.antdv.com/components/overview-cn/
+            component: 'Input',
+            required: true,
+            componentProps: {
+                placeholder: '请输入图像质量值'
+            }
+        },
+        {
+            field: 'config.add_face',
+            label: '表情添加',
+            bottomHelpMessage: '`#添加.....`功能',
+            component: 'Switch'
+        },
+        {
+            field: 'config.add_onlyMaster',
+            label: '表情添加仅主人',
+            bottomHelpMessage: '`#添加.....`功能仅能主人可用',
+            component: 'Switch'
+        },
+        {
+            field: 'config.poke',
+            label: '群聊戳一戳',
+            bottomHelpMessage: '戳一戳功能,随机图',
+            component: 'Switch'
+        },
+        {
+            field: 'config.poke_onlyBot',
+            label: '戳一戳仅Bot',
+            bottomHelpMessage: '若关闭,则所有人被戳都会触发随机图(嫌吵勿关闭)',
+            component: 'Switch'
+        },
+        {
+            label: '优先级配置',
+            // 第二个分组标记开始
+            component: 'SOFT_GROUP_BEGIN'
+        },
+        {
+            field: 'priority.weather',
+            label: '天气信息',
+            bottomHelpMessage: '`xx天气`,优先级越小越优先,可为负值,重启后生效',
+            component: 'Input',
+            required: true,
+            componentProps: {
+                placeholder: '请输入优先级'
+            }
+        },
+        {
+            field: 'priority.removeBackground',
+            label: '去背景',
+            bottomHelpMessage: '`#去背景`,优先级越小越优先,可为负值,重启后生效',
+            component: 'Input',
+            required: true,
+            componentProps: {
+                placeholder: '请输入优先级'
+            }
+        },
+        {
+            field: 'priority.LeaveMessages',
+            label: '留言',
+            bottomHelpMessage: '`#留言`,优先级越小越优先,可为负值,重启后生效',
+            component: 'Input',
+            required: true,
+            componentProps: {
+                placeholder: '请输入优先级'
+            }
+        },
+        {
+            field: 'priority.GroupPoke',
+            label: '群聊戳一戳',
+            bottomHelpMessage: '`戳一戳`,优先级越小越优先,可为负值,重启后生效',
+            component: 'Input',
+            required: true,
+            componentProps: {
+                placeholder: '请输入优先级'
+            }
+        },
+      ],
+      // 获取配置数据方法（用于前端填充显示数据）
+      async getConfigData () {
+        let data = {
+            config: await tool.readyaml('./plugins/San-plugin/config/config.yaml') ,
+            priority: await tool.readyaml('./plugins/San-plugin/config/priority.yaml') ,
+        }
+        //logger.info(data)
+        return data
+      },
+      // 设置配置的方法（前端点确定后调用的方法）
+      async setConfigData (data, { Result }) {
+        //logger.info(data)
+        // 将 newData 转换为嵌套对象
+        let NewData = {};
+        for (let [keyPath, value] of Object.entries(data)) {
+            //logger.info([keyPath, value])
+            if (keyPath === 'config.imgQuality' || keyPath.startsWith('priority.')) {
+                value = Number(value); // 尝试转换为数字
+              }
+            lodash.set(NewData, keyPath, value);
+
+        }
+        //logger.info(NewData)
+        try {
+            tool.objectToYamlFile(NewData.config,'./plugins/San-plugin/config/config.yaml')
+            tool.objectToYamlFile(NewData.priority,'./plugins/San-plugin/config/priority.yaml')
+            return Result.ok({}, '保存成功~')    
+        } catch (error) {
+            logger.error(error)
+            return Result.ok({}, '出现错误,请查看控制台日志~')  
+        }
+
+      }
+    }
+  }
+}
